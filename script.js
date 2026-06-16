d20a66f9bb397c5f5ea66e53af374af71642ee86
JJakob152 <jakob@hollingshaus.de>
2026-06-16 18:56:09 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const dateElement = document.getElementById("currentDate");
-
-if (dateElement) {
-    const today = new Date();
-
-    dateElement.textContent =
-        today.toLocaleDateString("de-DE");
-}
